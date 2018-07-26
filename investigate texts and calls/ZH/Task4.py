6c499c987d0248a0cd22fa9f4fcbe304d2dff9c3
Alex <543024658@qq.com>
2018-07-27 00:05:26 +0800

    作业完成

--- investigate texts and calls/ZH/Task4.py
+++ investigate texts and calls/ZH/Task4.py
@@ -12,6 +12,24 @@ with open('calls.csv', 'r') as f:
     reader = csv.reader(f)
     calls = list(reader)
 
+# 建立短信号码集合，包含发送号码和接收号码
+TextPhone = set([x[0] for x in texts]) | set([x[1] for x in texts])
+# 建立被叫号码集合
+CalledPhone = set([y[1] for y in calls])
+# 建立主叫号码集合
+CallPhone = set([y[0] for y in calls])
+
+# 根据特征 推销电话： 0短信 0被叫 all主叫
+SalesCalls = CallPhone - (TextPhone | CalledPhone)
+SalesCalls_list = list(SalesCalls)
+SalesCalls_list.sort()
+
+print("These numbers could be telemarketers: ")
+
+# 遍历打印SalesCalls_list
+for SalesCall in SalesCalls_list:
+    print(SalesCall)
+
 """
 任务4:
 电话公司希望辨认出可能正在用于进行电话推销的电话号码。
@@ -25,4 +43,3 @@ with open('calls.csv', 'r') as f:
 <list of numbers>
 电话号码不能重复，每行打印一条，按字典顺序排序后输出。
 """
-
