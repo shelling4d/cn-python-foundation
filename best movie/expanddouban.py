74f00b37b1b98eae7f9df3edef70a10cc6b54fac
Alex <543024658@qq.com>
2018-07-29 23:32:43 +0800

    Fale改成True

--- best movie/expanddouban.py
+++ best movie/expanddouban.py
@@ -5,8 +5,10 @@ import time
 url: the douban page we will get html from
 loadmore: whether or not click load more on the bottom 
 waittime: seconds the broswer will wait after intial load and 
-""" 
-def getHtml(url, loadmore = False, waittime = 2):
+"""
+
+
+def getHtml(url, loadmore=True, waittime=2):
     browser = webdriver.Chrome('chromedriver')
     browser.get(url)
     time.sleep(waittime)
@@ -21,8 +23,3 @@ def getHtml(url, loadmore = False, waittime = 2):
     html = browser.page_source
     browser.quit()
     return html
-
-# for test
-#url = "https://movie.douban.com/tag/#/?sort=S&range=9,10&tags=电影,剧情,美国"
-#html = getHtml(url)
-#print(html) 
